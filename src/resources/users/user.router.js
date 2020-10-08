f13be45e86479cab5304831da37809c95eccbd6f
Slava Torchik <torhik_slava@mail.ru>
2020-10-09 00:58:53 +0300

    feat: implement users resourses

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -2,10 +2,40 @@ const router = require('express').Router();
 const User = require('./user.model');
 const usersService = require('./user.service');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+router
+  .route('/')
+  .get(async (req, res) => {
+    const users = await usersService.getAll();
+    res.json(users.map(User.toResponse));
+  })
+  .post(async (req, res) => {
+    const user = await usersService.create(
+      new User({
+        name: req.body.name,
+        login: req.body.login,
+        password: req.body.password
+      })
+    );
+    res.json(User.toResponse(user));
+  });
+
+router
+  .route('/:id')
+  .get(async (req, res) => {
+    const user = await usersService.getById(req.params.id);
+    res.json(User.toResponse(user));
+  })
+  .put(async (req, res) => {
+    const user = await usersService.updateById(req.params.id, {
+      name: req.body.name,
+      login: req.body.login,
+      password: req.body.password
+    });
+    res.json(User.toResponse(user));
+  })
+  .delete(async (req, res) => {
+    await usersService.deleteById(req.params.id);
+    res.status(200).send('deleted!');
+  });
 
 module.exports = router;
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -2,4 +2,12 @@ const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const getById = id => usersRepo.getById(id);
+
+const create = user => usersRepo.create(user);
+
+const updateById = (id, obj) => usersRepo.updateById(id, obj);
+
+const deleteById = id => usersRepo.deleteById(id);
+
+module.exports = { getAll, getById, create, updateById, deleteById };
