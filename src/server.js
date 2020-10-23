136fcbe170a10c15a6f529ec6a26d2d5040bf74c
Slava Torchik <torhik_slava@mail.ru>
2020-10-24 00:17:36 +0300

    feat: add connection to mongoDB

--- src/server.js
+++ src/server.js
@@ -1,6 +1,24 @@
-const { PORT } = require('./common/config');
+const mongoose = require('mongoose');
+const { PORT, MONGO_CONNECTION_STRING } = require('./common/config');
 const app = require('./app');
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+async function run() {
+  try {
+    await mongoose.connect(MONGO_CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    });
+    mongoose.connection.db.dropDatabase();
+    console.log('Connected to mongo DB');
+
+    app.listen(PORT, () =>
+      console.log(`App is running on http://localhost:${PORT}`)
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+run();
