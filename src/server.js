8f686a91291cae8088e56339075508080af66208
Slava Torchik <torhik_slava@mail.ru>
2020-10-24 15:09:13 +0300

    refactor: add console comment for crosscheck

--- src/server.js
+++ src/server.js
@@ -14,7 +14,14 @@ async function run() {
     console.log('Connected to mongo DB');
 
     app.listen(PORT, () =>
-      console.log(`App is running on http://localhost:${PORT}`)
+      console.log(`App is running on http://localhost:${PORT}
+      !!!!
+      Инфа для проверяющих:
+      Старая реализация из проекта не удалялась! Просто изменены пути к файлам с новой реализацией.
+      Новую реализацию моделей смотреть в файлах с окончанием .mongoose.model.js, a не .model.js
+      Новую реализацию репозиториев смотреть в файлах с окончанием .mongoose.repository.js
+      Не перепутайте) Спасибо)
+      !!!!`)
     );
   } catch (err) {
     console.log(err);
